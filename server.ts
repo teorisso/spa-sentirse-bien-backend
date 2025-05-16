31fe256d50dde4f159f464cfaa8d4dde323dd20c
Gina <ginagrosso01@gmail.com>
2025-05-16 20:15:09 -0300

    update turnos

--- server.ts
+++ server.ts
@@ -17,7 +17,7 @@ app.use(cors(
   {
     origin: "*", 
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type']
+    allowedHeaders: ['Content-Type', 'Authorization'] // Added Authorization
   }
 ));
 
