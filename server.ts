ed509aa2a45ee169686bdbc92b0e78d8bac88009
Gina <ginagrosso01@gmail.com>
2025-05-16 11:27:02 -0300

    Remodificacion de server.ts

--- server.ts
+++ server.ts
@@ -17,7 +17,7 @@ app.use(cors(
   {
     origin: "*", 
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'] // Añadir 'Authorization' aquí
+    allowedHeaders: ['Content-Type'] // Añadir 'Authorization' aquí
   }
 ));
 
